462aa1a852a7c2f549d7bf4aabbf71a3ad92ba0d
WHOMENTORSDOTCOM <Rauhmel@WHOmentors.com>
2014-03-11 20:38:40 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mosaicdesignsbywenialee::Application.config.secret_key_base = '9b52f2a638bd589db8c95f05760572d6f58fb7a6cd15b2fa9ee06b6a66171c1b2ba13b528b3d8e1ed358d6968339caf30f2e6e45c655bd92193924dd63beff83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mosaicdesignsbywenialee::Application.config.session_store :cookie_store, key: '_mosaicdesignsbywenialee_session'
