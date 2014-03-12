12ca4beb0ed55d71bfcb7db0724deedb488acb25
WHOMENTORSDOTCOM <Rauhmel@WHOmentors.com>
2014-03-12 11:07:59 -0700

    Added Seller Page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Mosaicdesignsbywenialee::Application.routes.draw do
 
   get "pages/about"
   get "pages/contact"
+  get 'seller' => "listings#seller"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
