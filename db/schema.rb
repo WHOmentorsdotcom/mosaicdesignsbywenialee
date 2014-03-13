fed60b3d33e643dab40f4955615368845016ab93
WHOMENTORSDOTCOM <Rauhmel@WHOmentors.com>
2014-03-12 19:13:57 -0700

    Fixed Errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312223607) do
+ActiveRecord::Schema.define(version: 20140313004016) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20140312223607) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: true do |t|
